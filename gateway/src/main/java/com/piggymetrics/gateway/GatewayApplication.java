b48ef38f37f4b4fa38a28463958e207586d9cdbb
Zhe Li <zlhe@microsoft.com>
2020-04-14 14:27:22 +0800

    update

--- gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
@@ -5,35 +5,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-public class LeakFactory
-{//Just so that we have some data to leak
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
     int myID = 0;
-// Necessary because our Leak class is non-static
+    // Necessary because our Leak class is non-static
     public Leak createLeak()
     {
         return new Leak();
     }
 
-// Mass Manufactured Leak class
+    // Mass Manufactured Leak class
     public class Leak
     {//Again for a little data.
        int size = 1;
     }
-}
 
-@SpringBootApplication
-@EnableDiscoveryClient
-@EnableZuulProxy
-public class GatewayApplication {
-
-	public Leak[] myHoles;
+	public static Leak[] myHoles;
 
 	public static void main(String[] args) {
 		myHoles = new Leak[1000];
 
-        for (int i = 0; i++; i<1000)
+        for (int i = 0; i < 1000; i++)
         {//Store them in the class member
-            myHoles[i] = new LeakFactory().createLeak();
+            myHoles[i] = new GatewayApplication().createLeak();
         }
 		
 		SpringApplication.run(GatewayApplication.class, args);
